9a4c3e8ed0d43a84f56d62a54636083a05dd1a9a
ScriptedPixelsLtd <kam@scriptedpixels.co.uk>
2020-09-25 00:08:09 +0100

    Updates blog layout to incude Nav and fix layout on mobile with flexbox

--- layouts/blog.vue
+++ layouts/blog.vue
@@ -1,22 +1,26 @@
 <template>
-  <div class="container">
-    <h1>The blog layout:</h1>
+  <div>
+    <TheNavbar />
 
-    <div class="flex-layout">
-      <div class="blog-content">
-        <nuxt />
+    <div class="container">
+      <h1>The blog layout:</h1>
+
+      <div class="flex-layout">
+        <div class="blog-content">
+          <nuxt />
+        </div>
+
+        <aside>
+          <!-- <h3>Related posts:</h3>
+          <ul>
+            <li v-for="related in relatedPosts" :key="related.id">
+              <nuxt-link :to="'/blog/' + related.id">
+                {{ related.title }}
+              </nuxt-link>
+            </li>
+          </ul> -->
+        </aside>
       </div>
-
-      <aside>
-        <!-- <h3>Related posts:</h3>
-        <ul>
-          <li v-for="related in relatedPosts" :key="related.id">
-            <nuxt-link :to="'/blog/' + related.id">
-              {{ related.title }}
-            </nuxt-link>
-          </li>
-        </ul> -->
-      </aside>
     </div>
   </div>
 </template>
@@ -28,16 +32,19 @@ export default {}
 <style scoped>
 .flex-layout {
   justify-content: space-between;
+  flex-direction: row;
   margin: 30px 0 0;
+  flex-wrap: wrap;
   display: flex;
 }
 .blog-content {
   margin: 0 20px 0 0;
-  flex: 1 0 300px;
+  flex: 1 0 420px;
 }
 aside {
   background-color: rgba(255, 255, 255, 0.1);
-  height: 100vh;
+  min-height: 100px;
   min-width: 300px;
+  flex: 1 1 300px;
 }
 </style>
