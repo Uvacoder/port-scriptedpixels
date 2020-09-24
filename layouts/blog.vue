dd896938306b414c78a8f6ed7d62b6b970c2d35d
ScriptedPixelsLtd <kam@scriptedpixels.co.uk>
2020-09-24 23:45:55 +0100

    Adds a new blog layout for blog posts

---
+++ layouts/blog.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="container">
+    <h1>The blog layout:</h1>
+
+    <div class="flex-layout">
+      <div class="blog-content">
+        <nuxt />
+      </div>
+
+      <aside>
+        <!-- <h3>Related posts:</h3>
+        <ul>
+          <li v-for="related in relatedPosts" :key="related.id">
+            <nuxt-link :to="'/blog/' + related.id">
+              {{ related.title }}
+            </nuxt-link>
+          </li>
+        </ul> -->
+      </aside>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+.flex-layout {
+  justify-content: space-between;
+  margin: 30px 0 0;
+  display: flex;
+}
+.blog-content {
+  margin: 0 20px 0 0;
+  flex: 1 0 300px;
+}
+aside {
+  background-color: rgba(255, 255, 255, 0.1);
+  height: 100vh;
+  min-width: 300px;
+}
+</style>
