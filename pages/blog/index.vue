672e39c90361c895486c14b21f89955e8601e6e5
ScriptedPixelsLtd <kam@scriptedpixels.co.uk>
2020-09-25 00:08:23 +0100

    Update blog index page to show blog posts

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,9 +1,31 @@
 <template>
-  <h1>Blog: A list of the most recent blog posts, in bactches of 10</h1>
+  <div>
+    <h1>Blog: A list of the most recent blog posts, in bactches of 10</h1>
+    <div class="links">
+      <h3>Latest posts from the blog</h3>
+      <ul>
+        <li v-for="post in posts" :key="post.id">
+          <nuxt-link :to="`/blog/${post.id}`">
+            {{ post.title }}
+          </nuxt-link>
+        </li>
+      </ul>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {}
+export default {
+  asyncData({ $axios }) {
+    return $axios.get('http://jsonplaceholder.typicode.com/posts?_limit=20').then((response) => {
+      return { posts: response.data }
+    })
+  },
+}
 </script>
 
-<style></style>
+<style>
+.links {
+  margin: 30px auto 0;
+}
+</style>
