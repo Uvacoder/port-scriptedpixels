df4d25c48ec41828e2a7f1d2db9c0c9f2f26912c
ScriptedPixelsLtd <kam@scriptedpixels.co.uk>
2020-09-25 00:45:32 +0100

    Adding ‘exact’ to nav to stop home being highlighted in nav on all other pages incorrectly

--- components/TheNavbar.vue
+++ components/TheNavbar.vue
@@ -1,6 +1,6 @@
 <template>
   <nav class="container nav">
-    <nuxt-link :to="{ name: 'index' }">Home</nuxt-link>
+    <nuxt-link exact :to="{ name: 'index' }">Home</nuxt-link>
     <nuxt-link :to="{ name: 'about' }">About</nuxt-link>
     <nuxt-link :to="{ name: 'work' }">Work</nuxt-link>
     <nuxt-link :to="{ name: 'blog' }">Blog</nuxt-link>
