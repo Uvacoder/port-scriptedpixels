50f3c019ebb25856fea8ae26a1e08efae5d343c3
ScriptedPixelsLtd <kam@scriptedpixels.co.uk>
2020-09-25 12:55:24 +0100

    Adds an error layout page

---
+++ layouts/error.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+    <div v-if="error.statusCode === 404">
+      <h1>Page not found</h1>
+      <p>Test</p>
+    </div>
+    <h1 v-else>An error has occurred</h1>
+    <nuxt-link to="/">Home Page</nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['error'],
+}
+</script>
+
+<style></style>
