696ca5d457dcba3ce6a3eae4bbcc10b445b0fbc6
ScriptedPixelsLtd <kam@scriptedpixels.co.uk>
2020-09-25 17:11:26 +0100

    Update name

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,7 +10,7 @@ export default {
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: 'scripted-pixels-ltd',
+    title: 'Scripted Pixels Ltd',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
