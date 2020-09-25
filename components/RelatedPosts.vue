664863f12cff3e59eca3b311fbf1ac98156c91b0
ScriptedPixelsLtd <kam@scriptedpixels.co.uk>
2020-09-25 17:11:55 +0100

    Adds related post component

---
+++ components/RelatedPosts.vue
@@ -0,0 +1,18 @@
+<template>
+  <ul>
+    <li>Related Posts to go here</li>
+    <!-- <li v-for="related in relatedPosts" :key="related.id">
+      <nuxt-link :to="'/blog/' + related.id">
+        {{ related.title }}
+      </nuxt-link>
+    </li> -->
+  </ul>
+</template>
+
+<script>
+/* eslint-disable no-alert, no-console */
+export default {}
+/* eslint-disable no-alert, no-console */
+</script>
+
+<style scoped></style>
