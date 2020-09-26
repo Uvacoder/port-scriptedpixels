a6ec03c6e20685ec125b62083cd7bac1d1cbc8eb
ScriptedPixelsLtd <kam@scriptedpixels.co.uk>
2020-09-26 20:53:50 +0100

    Adds get and set post/posts

--- store/index.js
+++ store/index.js
@@ -1,3 +1,26 @@
-export const state = () => ({})
+import axios from 'axios'
 
-export const getters = {}
+export const state = () => ({
+  posts: [],
+  post: {},
+})
+
+export const mutations = {
+  SET_POSTS(state, posts) {
+    state.posts = posts
+  },
+  SET_POST(state, post) {
+    state.post = post
+  },
+}
+
+export const actions = {
+  async GET_POSTS({ commit }) {
+    const { data } = await axios.get('http://jsonplaceholder.typicode.com/posts?_limit=10')
+    commit('SET_POSTS', data)
+  },
+  async GET_POST({ commit }, id) {
+    const { data } = await axios.get(`http://jsonplaceholder.typicode.com/posts/${id}`)
+    commit('SET_POST', data)
+  },
+}
