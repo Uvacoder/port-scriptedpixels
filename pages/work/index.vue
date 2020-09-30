90d08283b77830378e8d641bedb1f68bdec81352
ScriptedPixelsLtd <kam@scriptedpixels.co.uk>
2020-10-01 00:46:35 +0100

    shows posts from Wordpress API from WORK category only

--- pages/work/index.vue
+++ pages/work/index.vue
@@ -1,9 +1,29 @@
 <template>
-  <h1>Work: home page to list out most recent projects I've worked on</h1>
+  <div>
+    <h1>Work: home page to list out most recent projects I've worked on</h1>
+    <ul class="posts-list">
+      <li v-for="project in projects" :key="project.id">
+        <h2>{{ project.title.rendered }}</h2>
+        <small v-html="project.excerpt.rendered"></small>
+        <nuxt-link :to="`/work/${project.id}`">Read more...</nuxt-link>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
-export default {}
+import { mapState } from 'vuex'
+
+export default {
+  async fetch({ store }) {
+    await store.dispatch('work/GET_PROJECTS')
+  },
+  computed: {
+    projects() {
+      return this.$store.state.work.projects
+    },
+  },
+}
 </script>
 
 <style></style>
