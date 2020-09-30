1819e5172686a1f69cb8d4052f090e3613888e18
ScriptedPixelsLtd <kam@scriptedpixels.co.uk>
2020-09-30 21:25:04 +0100

    remove blog posts list from home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,30 +1,12 @@
 <template>
   <div class="container">
     <h1>Scripted Pixels</h1>
-
-    <div class="links">
-      <h3>Latest 10 posts from the blog</h3>
-      <ul class="posts-list">
-        <li v-for="post in posts" :key="post.id" class="posts-list-link">
-          <nuxt-link :to="`/blog/${post.id}`">
-            {{ post.title }}
-          </nuxt-link>
-        </li>
-      </ul>
-    </div>
+    <p>New portfolio home page with some lovely info...</p>
   </div>
 </template>
 
 <script>
-import { mapState } from 'vuex'
-
 export default {
-  async fetch({ store }) {
-    await store.dispatch('GET_POSTS')
-  },
-  computed: {
-    ...mapState(['posts']),
-  },
   head() {
     return {
       title: 'Scripted Pixels | Home Page',
