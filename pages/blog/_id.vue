5a710d3881af8f2d670a56593ce6a2a963bffd2c
ScriptedPixelsLtd <kam@scriptedpixels.co.uk>
2020-09-30 21:53:57 +0100

    Blog page includes related posts component
    1. pass in posts in to related posts with current post filterd out
    2.

--- pages/blog/_id.vue
+++ pages/blog/_id.vue
@@ -1,28 +1,62 @@
 <template>
-  <article class="container">
-    <h1 class="title">{{ currentPost.title }}</h1>
-    <p>
-      <small>ID: {{ currentPost.id }}</small>
-    </p>
-    <p>
-      <small>User: {{ currentPost.userId }}</small>
-    </p>
-    <p>{{ currentPost.body }}</p>
-  </article>
+  <div class="flex-layout">
+    <article class="container blog-content">
+      <h1 class="title">{{ currentPost.title }}</h1>
+      <p>
+        <small>ID: {{ currentPost.id }}</small>
+      </p>
+      <p>
+        <small>User: {{ currentPost.userId }}</small>
+      </p>
+      <p>{{ currentPost.body }}</p>
+    </article>
+
+    <aside>
+      <h3>Related posts:</h3>
+      <RelatedPosts :posts="relatedPosts" />
+    </aside>
+  </div>
 </template>
 
 <script>
 import { mapState } from 'vuex'
+import RelatedPosts from '@/components/RelatedPosts'
 
 export default {
+  components: {
+    RelatedPosts,
+  },
   async fetch({ store, params }) {
+    await store.dispatch('GET_POSTS', params.id)
     await store.dispatch('GET_CURRENT_POST', params.id)
   },
   computed: {
-    ...mapState(['currentPost']),
+    ...mapState(['currentPost', 'posts']),
+    relatedPosts() {
+      return this.posts.filter((p) => p.id !== this.currentPost.id)
+    },
   },
   layout: 'blog',
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+.flex-layout {
+  justify-content: space-between;
+  flex-direction: row;
+  margin: 30px 0 0;
+  flex-wrap: wrap;
+  display: flex;
+}
+
+.blog-content {
+  margin: 0 20px 0 0;
+  flex: 1 1 420px;
+}
+
+aside {
+  min-height: 100px;
+  min-width: 300px;
+  flex: 1 1 300px;
+}
+</style>
