36787b2481d79dfa0342e5f803b80b987485e300
ScriptedPixelsLtd <kam@scriptedpixels.co.uk>
2020-09-30 23:40:34 +0100

    attempting to get plugins working for axios logging

--- nuxt.config.js
+++ nuxt.config.js
@@ -25,7 +25,7 @@ export default {
   css: ['@/assets/css/globalCss.css'],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-  plugins: ['~/plugins/axios.js'],
+  plugins: ['~/plugins/axios'],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
   components: true,
--- plugins/axios.js
+++ plugins/axios.js
@@ -1,6 +1,13 @@
-export default function ({ $axios }) {
+/* eslint-disable no-console */
+export default function ({ $axios, redirect }) {
   $axios.onRequest((config) => {
-    // eslint-disable-next-line no-console
-    console.log(`Making a request to ${config.url}`)
+    console.log('Making request to ' + config.url)
+  })
+
+  $axios.onError((error) => {
+    const code = parseInt(error.response && error.response.status)
+    if (code === 400) {
+      redirect('/400')
+    }
   })
 }
