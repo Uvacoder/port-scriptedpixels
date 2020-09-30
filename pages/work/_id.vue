9cae13b12e6d883f9005299187cb25b9f3a9f3ee
ScriptedPixelsLtd <kam@scriptedpixels.co.uk>
2020-10-01 00:47:03 +0100

    work project page to show project copy

---
+++ pages/work/_id.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="flex-layout">
+    <article class="container blog-content">
+      <h1 class="title">{{ currentProject.title.rendered }}</h1>
+      <div v-html="currentProject.content.rendered"></div>
+    </article>
+
+    <!-- <aside>
+      <h3>Related posts:</h3>
+      <RelatedPosts :posts="relatedPosts" />
+    </aside> -->
+  </div>
+</template>
+
+<script>
+export default {
+  async fetch({ store, params }) {
+    await store.dispatch('work/GET_PROJECTS')
+    await store.dispatch('work/GET_CURRENT_PROJECT', params.id)
+  },
+  computed: {
+    currentProject() {
+      return this.$store.state.work.currentProject
+    },
+  },
+}
+</script>
+
+<style></style>
