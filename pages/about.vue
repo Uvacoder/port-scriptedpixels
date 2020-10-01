20eeee13686f981d64df904466b137fc62ab8fb4
ScriptedPixelsLtd <kam@scriptedpixels.co.uk>
2020-10-02 00:28:24 +0100

    Updates About page to use correct CSS for page

--- pages/about.vue
+++ pages/about.vue
@@ -1,33 +1,35 @@
 <template>
-  <div>
-    <h1>About: about page</h1>
-    <h2>Netlify forms test</h2>
-    <form name="contact" method="POST" data-netlify="true" data-netlify-honeypot="bot-field" action="/success">
-      <input type="hidden" name="form-name" value="contact" />
-      <p>
-        <label for="name">Your Name:</label>
-        <input type="text" name="name" />
-      </p>
-      <p>
-        <label for="email">Your Email:</label>
-        <input type="email" name="email" />
-      </p>
-      <p>
-        <label for="role[]">Your Role:</label>
-        <select name="role[]" multiple>
-          <option value="leader">Leader</option>
-          <option value="follower">Follower</option>
-        </select>
-      </p>
-      <p>
-        <label for="message">Message:</label>
-        <textarea name="message"></textarea>
-      </p>
-      <p>
-        <button type="submit">Send</button>
-      </p>
-    </form>
-  </div>
+  <section class="section grid-content">
+    <div class="container">
+      <h1 class="title">About</h1>
+      <h2>Netlify forms test</h2>
+      <form name="contact" method="POST" data-netlify="true" data-netlify-honeypot="bot-field" action="/success">
+        <input type="hidden" name="form-name" value="contact" />
+        <p>
+          <label for="name">Your Name:</label>
+          <input type="text" name="name" />
+        </p>
+        <p>
+          <label for="email">Your Email:</label>
+          <input type="email" name="email" />
+        </p>
+        <p>
+          <label for="role[]">Your Role:</label>
+          <select name="role[]" multiple>
+            <option value="leader">Leader</option>
+            <option value="follower">Follower</option>
+          </select>
+        </p>
+        <p>
+          <label for="message">Message:</label>
+          <textarea name="message"></textarea>
+        </p>
+        <p>
+          <button type="submit">Send</button>
+        </p>
+      </form>
+    </div>
+  </section>
 </template>
 
 <script>
