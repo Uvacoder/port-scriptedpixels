066a5ab162be2c78fa43c140c660e3b873eb7197
ScriptedPixelsLtd <kam@scriptedpixels.co.uk>
2020-10-02 00:27:43 +0100

    Adds a new layout for the home page

---
+++ layouts/homepage.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <section class="hero is-fullheight-with-navbar">
+      <Navigation />
+      <div class="hero-body">
+        <div class="container">
+          <div class="container has-text-centered">
+            <img src="~/assets/images/scriptedPixelsLogo.png" alt="Scripted Pixels Logo" />
+            <h2 class="subtitle">Design, Develop, Deliver</h2>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="hero">
+      <Nuxt />
+    </section>
+  </div>
+</template>
+
+<script>
+import Navigation from '@/components/TheNavbar'
+
+export default {
+  components: {
+    Navigation,
+  },
+}
+</script>
+
+<style scoped></style>
