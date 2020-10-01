e6dc073bccff24e4be23fb7bf9aba53091d8ccf9
ScriptedPixelsLtd <kam@scriptedpixels.co.uk>
2020-10-02 00:28:57 +0100

    Updates default page layout to use Bulma classes

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,19 +1,19 @@
 <template>
-  <div>
+  <div class="grid-container">
     <Navigation />
-
-    <div class="container">
-      <Nuxt />
-    </div>
+    <Nuxt />
+    <TheFooter />
   </div>
 </template>
 
 <script>
 import Navigation from '@/components/TheNavbar'
+import TheFooter from '@/components/TheFooter'
 
 export default {
   components: {
     Navigation,
+    TheFooter,
   },
 }
 </script>
