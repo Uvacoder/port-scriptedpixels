6e19afd39602b0ba97bb2f2196f319d3fbf7ada2
ScriptedPixelsLtd <kam@scriptedpixels.co.uk>
2020-10-01 15:20:10 +0100

    Correct page location

--- pages/about.vue
+++ pages/about.vue
@@ -2,7 +2,7 @@
   <div>
     <h1>About: about page</h1>
     <h2>Netlify forms test</h2>
-    <form name="contact" method="POST" data-netlify="true" data-netlify-honeypot="bot-field" action="/pages/success">
+    <form name="contact" method="POST" data-netlify="true" data-netlify-honeypot="bot-field" action="/success">
       <input type="hidden" name="form-name" value="ask-question" />
       <p>
         <label for="name">Your Name:</label>
