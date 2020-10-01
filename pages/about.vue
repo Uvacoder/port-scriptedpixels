9a34822b673783f00506346fcd4b26ec9f3863af
ScriptedPixelsLtd <kam@scriptedpixels.co.uk>
2020-10-01 14:45:34 +0100

    Adds Netlify Form

--- pages/about.vue
+++ pages/about.vue
@@ -1,5 +1,31 @@
 <template>
-  <h1>About: about page</h1>
+  <div>
+    <h1>About: about page</h1>
+    <h2>Netlify forms test</h2>
+    <form name="contact" method="POST" data-netlify="true" data-netlify-honeypot="bot-field">
+      <p>
+        <label>Your Name: <input type="text" name="name" /></label>
+      </p>
+      <p>
+        <label>Your Email: <input type="email" name="email" /></label>
+      </p>
+      <p>
+        <label
+          >Your Role:
+          <select name="role[]" multiple>
+            <option value="leader">Leader</option>
+            <option value="follower">Follower</option>
+          </select></label
+        >
+      </p>
+      <p>
+        <label>Message: <textarea name="message"></textarea></label>
+      </p>
+      <p>
+        <button type="submit">Send</button>
+      </p>
+    </form>
+  </div>
 </template>
 
 <script>
