12e511e297aa63079ae64caa3d62b9ca3fc2615a
ScriptedPixelsLtd <kam@scriptedpixels.co.uk>
2020-10-01 10:52:00 +0100

    adds published date

--- pages/work/_id.vue
+++ pages/work/_id.vue
@@ -2,6 +2,7 @@
   <div class="flex-layout">
     <article class="container blog-content">
       <h1 class="title">{{ currentProject.title.rendered }}</h1>
+      <small>Published on: {{ currentProject.date }}</small>
       <div v-html="currentProject.content.rendered"></div>
     </article>
 
