4d43b64867707754e73ba6a402780e213ade1a82
ScriptedPixelsLtd <kam@scriptedpixels.co.uk>
2020-10-07 00:53:14 +0100

    Creates a component for listing out posts

---
+++ components/ListItems.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="card">
+    <div class="card-content">
+      <h4 class="title is-size-4">
+        <nuxt-link :to="`${postData.path}`">
+          {{ postData.title }}
+        </nuxt-link>
+      </h4>
+      <p class="subtsitle">Date pusblished: {{ postData.date }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['postData'],
+}
+</script>
+
+<style></style>
