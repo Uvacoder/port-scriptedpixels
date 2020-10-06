ad4cc96b5f6823ca85d58e51b3ddea36e7ebed55
ScriptedPixelsLtd <kam@scriptedpixels.co.uk>
2020-10-07 00:16:57 +0100

    rename _id to _slug for work posts

---
+++ pages/work/_slug.vue
@@ -0,0 +1,13 @@
+<template>
+  <nuxt-content :document="workPost" />
+</template>
+
+<script>
+export default {
+  async asyncData({ $content, params }) {
+    const workPost = await $content('work', params.slug).fetch()
+
+    return { workPost }
+  },
+}
+</script>
