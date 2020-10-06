c9ca541311221fa9d5ea27e0d926d3e22699f6fc
ScriptedPixelsLtd <kam@scriptedpixels.co.uk>
2020-10-06 23:47:53 +0100

    rename blog/_id to blog_slug for easier reading

---
+++ pages/blog/_slug.vue
@@ -0,0 +1,19 @@
+<template>
+  <section class="section grid-content">
+    <div class="container">
+      <nuxt-content :document="blogPost" />
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  async asyncData({ $content, params }) {
+    const blogPost = await $content('blog', params.slug).fetch()
+
+    return { blogPost }
+  },
+}
+</script>
+
+<style scoped></style>
