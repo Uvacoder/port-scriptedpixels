0634525da771681d6d40b073cca4fcc1b6a45cd0
ScriptedPixelsLtd <kam@scriptedpixels.co.uk>
2020-10-06 21:29:26 +0100

    Adds content blocks to home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,36 @@
 <template>
-  <div>
-    <section class="hero is-medium is-link">
-      <div class="hero-body">
-        <div class="container">
-          <p>the next hero area</p>
-        </div>
+  <div class="container py-6 is-fluid">
+    <div class="columns">
+
+      <div class="column has-background-link-light m-2">
+          <h3 class="title">Design</h3>
+          <h4 class="subtitle">Wireframes, Sketch, Prototypes</h4>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum nisi corporis eveniet ipsam blanditiis in amet,
+            eligendi, optio earum totam similique eius nostrum. Voluptatem facilis possimus saepe molestias nisi cum.
+          </p>
       </div>
-    </section>
-    <section class="hero is-large is-primary">
-      <div class="hero-body">
-        <div class="container">
-          <p>the next hero area</p>
-        </div>
+
+      <div class="column has-background-primary-light m-2">
+          <h3 class="title">Develop</h3>
+          <h4 class="subtitle">Coding, iterate, create</h4>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum nisi corporis eveniet ipsam blanditiis in amet,
+            eligendi, optio earum totam similique eius nostrum. Voluptatem facilis possimus saepe molestias nisi cum.
+          </p>
       </div>
-    </section>
+
+      <div class="column has-background-warning-light m-2">
+          <h3 class="title">Deliver</h3>
+          <h4 class="subtitle">Deploy, release, production</h4>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum nisi corporis eveniet ipsam blanditiis in amet,
+            eligendi, optio earum totam similique eius nostrum. Voluptatem facilis possimus saepe molestias nisi cum.
+          </p>
+      </div>
+    </div>
   </div>
+</div>
 </template>
 
 <script>
@@ -28,4 +44,11 @@ export default {
 }
 </script>
 
-<style></style>
+<style scoped>
+.tile {
+  height: 20vh;
+}
+.columns {
+  align-items: stretch;
+}
+</style>
