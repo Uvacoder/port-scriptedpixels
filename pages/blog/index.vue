a207b97680fb1594fc5a01dea3bf7259568a56d0
ScriptedPixelsLtd <kam@scriptedpixels.co.uk>
2020-10-07 00:04:09 +0100

    loops through each blog post & creates nuxt-links to each post :)

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -3,10 +3,10 @@
     <h1 class="title">Latest posts from the blog</h1>
     <div class="links">
       <ul class="posts-list">
-        <li v-for="post in blogPosts" :key="post.date" class="posts-list-link">
-          <!-- <nuxt-link :to="`/blog/${post.id}`"> -->
-          {{ post.title }}
-          <!-- </nuxt-link> -->
+        <li v-for="post in blogPosts" :key="post.slug" class="posts-list-link">
+          <nuxt-link :to="`/blog/${post.slug}`">
+            {{ post.title }}
+          </nuxt-link>
         </li>
       </ul>
     </div>
@@ -14,21 +14,16 @@
 </template>
 
 <script>
-// import { mapState } from 'vuex'
-
 export default {
-  // async fetch({ store }) {
-  //   await store.dispatch('GET_POSTS')
-  // },
   async asyncData({ $content, params }) {
-    const blogPosts = await $content('blog', params.slug).only(['title', 'description']).sortBy('date', 'asc').fetch()
+    const blogPosts = await $content('blog', params.slug)
+      .only(['title', 'date', 'description', 'slug'])
+      .sortBy('createdAt', 'desc')
+      .fetch()
 
     return {
       blogPosts,
     }
   },
-  // computed: {
-  //   ...mapState(['posts']),
-  // },
 }
 </script>
