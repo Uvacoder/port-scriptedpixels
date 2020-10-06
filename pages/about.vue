e59c3d5c46141838c8977d5d89b5dde6d73d71a5
ScriptedPixelsLtd <kam@scriptedpixels.co.uk>
2020-10-07 00:27:56 +0100

    Update contact for on about page with correct styling

--- pages/about.vue
+++ pages/about.vue
@@ -9,35 +9,46 @@
     </div>
 
     <div class="content">
+      <p>Have this on the right hand side on desktop</p>
       <h4 class="subtitle">Contact Scripted Pixels?</h4>
     </div>
 
     <div class="content">
       <h4>Netlify forms test</h4>
+
       <form name="contact" method="POST" data-netlify="true" data-netlify-honeypot="bot-field" action="/success">
-        <input type="hidden" name="form-name" value="contact" />
-        <p>
-          <label for="name">Your Name:</label>
-          <input type="text" name="name" />
-        </p>
-        <p>
-          <label for="email">Your Email:</label>
-          <input type="email" name="email" />
-        </p>
-        <p>
-          <label for="role[]">Your Role:</label>
-          <select name="role[]" multiple>
-            <option value="leader">Leader</option>
-            <option value="follower">Follower</option>
-          </select>
-        </p>
-        <p>
-          <label for="message">Message:</label>
-          <textarea name="message"></textarea>
-        </p>
-        <p>
-          <button type="submit">Send</button>
-        </p>
+        <fieldset>
+          <input type="hidden" name="form-name" value="contact" />
+
+          <div class="field">
+            <label for="name" class="label">Name</label>
+            <div class="control">
+              <input class="input" type="text" name="name" placeholder="Text input" />
+              <p class="help">This is a help text</p>
+            </div>
+          </div>
+
+          <div class="field">
+            <label for="email" class="label">Your Email:</label>
+            <div class="control">
+              <input class="input" type="email" name="email" placeholder="Text input" />
+              <p class="help">This is a help text</p>
+            </div>
+          </div>
+
+          <div class="field">
+            <label for="message" class="label">Message</label>
+            <div class="control">
+              <textarea class="textarea" name="message" placeholder="Your message"></textarea>
+            </div>
+          </div>
+
+          <div class="field is-grouped">
+            <div class="control">
+              <button type="submit" class="button is-primary">Submit</button>
+            </div>
+          </div>
+        </fieldset>
       </form>
     </div>
   </div>
