0c3c5afa4232a034222e9a8b558f5031b247ee27
ScriptedPixelsLtd <kam@scriptedpixels.co.uk>
2020-10-08 22:11:10 +0100

    adds prism as plugin

--- nuxt.config.js
+++ nuxt.config.js
@@ -28,7 +28,7 @@ export default {
   css: ['~/assets/scss/scriptedpixels.scss'],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-  plugins: ['~/plugins/axios'],
+  plugins: ['@/plugins/axios', { src: '@/plugins/prism', ssr: false }],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
   components: true,
