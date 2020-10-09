78afd7dd85a0ee5bf4040f8d85fcf7501c135c39
ScriptedPixelsLtd <kam@scriptedpixels.co.uk>
2020-10-09 10:01:10 +0100

    import prism and run on mounted

--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -3,11 +3,16 @@
 </template>
 
 <script>
+import Prism from '~/plugins/prism'
+
 export default {
   async asyncData({ $content, params }) {
     const blogPost = await $content('blog', params.slug).fetch()
 
     return { blogPost }
+  },
+  mounted() {
+    Prism.highlightAll()
   },
 }
 </script>
