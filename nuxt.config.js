f5b004a5de46896efe230430ac13028f71d9cd89
ScriptedPixelsLtd <kam@scriptedpixels.co.uk>
2020-10-09 12:23:29 +0100

    remove plugin for prism - not needed!

--- nuxt.config.js
+++ nuxt.config.js
@@ -28,7 +28,7 @@ export default {
   css: ['~/assets/scss/scriptedpixels.scss'],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-  plugins: ['~/plugins/axios', '~/plugins/prism'],
+  plugins: ['~/plugins/axios'],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
   components: true,
