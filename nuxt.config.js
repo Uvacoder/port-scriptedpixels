88577745dc022d11202b008956b5a1f32e91cb7f
ScriptedPixelsLtd <kam@scriptedpixels.co.uk>
2020-10-09 12:08:37 +0100

    Adds PurgeCSS whitelist to stop codeblocs from being unstyled!

--- nuxt.config.js
+++ nuxt.config.js
@@ -63,5 +63,31 @@ export default {
       },
     },
   },
-  purgeCSS: {},
+  purgeCSS: {
+    enabled: true, // True means it's always on in dev/build/generate mode
+    // The two below options achieve the happy medium between Prism.js and PurgeCSS
+    // If you're having trouble with code highlighting, try adding the pertinent clases to the whitelist
+    whitelistPatternsChildren: [/token$/],
+    whitelist: [
+      'pre',
+      'code',
+      'prism',
+      'line-numbers',
+      'tag',
+      'toolbar-item',
+      'toolbar',
+      'code-toolbar',
+      'span',
+      'button',
+      'line-numbers-rows',
+      'url-link',
+      'attr-name',
+      'attr-value',
+      'punctuation',
+      'keyword',
+      'keyword-let',
+      'operator',
+      'string',
+    ],
+  },
 }
