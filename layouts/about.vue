b1f55f33714e60ecc9dbf08e19274b0c379cc3a6
ScriptedPixelsLtd <kam@scriptedpixels.co.uk>
2020-10-19 20:49:55 +0100

    about page layout

---
+++ layouts/about.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <Nuxt />
+    <Navigation />
+  </div>
+</template>
+
+<script>
+import Navigation from '@/components/TheNavbar'
+
+export default {
+  components: {
+    Navigation,
+  },
+}
+</script>
+
+<style scoped></style>
--- pages/about.vue
+++ pages/about.vue
@@ -1,61 +1,72 @@
 <template>
   <div>
-    <div class="content">
-      <h1 class="title is-spaced">About Scripted Pixels</h1>
+    <section class="hero is-light">
+      <div class="hero-body">
+        <div class="container">
+          <h3 class="title is-size-3 is-spaced p-0 mb-0">About</h3>
+          <h4 class="subtitle p-0 mb-0">.....</h4>
+        </div>
+      </div>
+    </section>
 
-      <h4 class="subtitle">Who's behind Scripted Pixels?</h4>
-
-      <p class="">I'm a ...</p>
-    </div>
-
-    <div class="content">
-      <p>Have this on the right hand side on desktop</p>
-      <h4 class="subtitle">Contact Scripted Pixels?</h4>
-    </div>
-
-    <div class="content">
-      <h4>Netlify forms test</h4>
-
-      <form name="contact" method="POST" data-netlify="true" data-netlify-honeypot="bot-field" action="/success">
-        <fieldset>
-          <input type="hidden" name="form-name" value="contact" />
-
-          <div class="field">
-            <label for="name" class="label">Name</label>
-            <div class="control">
-              <input class="input" type="text" name="name" placeholder="Text input" />
-              <p class="help">This is a help text</p>
-            </div>
+    <section class="section grid-content container">
+      <div class="columns">
+        <div class="column">
+          <div class="content">
+            <p>Have this on the right hand side on desktop</p>
+            <h4 class="subtitle">Contact Scripted Pixels?</h4>
           </div>
+        </div>
 
-          <div class="field">
-            <label for="email" class="label">Your Email:</label>
-            <div class="control">
-              <input class="input" type="email" name="email" placeholder="Text input" />
-              <p class="help">This is a help text</p>
-            </div>
-          </div>
+        <div class="column is-two-fifths">
+          <div class="content">
+            <h4>Netlify forms test</h4>
 
-          <div class="field">
-            <label for="message" class="label">Message</label>
-            <div class="control">
-              <textarea class="textarea" name="message" placeholder="Your message"></textarea>
-            </div>
-          </div>
+            <form name="contact" method="POST" data-netlify="true" data-netlify-honeypot="bot-field" action="/success">
+              <fieldset>
+                <input type="hidden" name="form-name" value="contact" />
 
-          <div class="field is-grouped">
-            <div class="control">
-              <button type="submit" class="button is-primary">Submit</button>
-            </div>
+                <div class="field">
+                  <label for="name" class="label">Name</label>
+                  <div class="control">
+                    <input class="input" type="text" name="name" placeholder="Text input" aria-label="Your name" />
+                    <p class="help">This is a help text</p>
+                  </div>
+                </div>
+
+                <div class="field">
+                  <label for="email" class="label">Your Email:</label>
+                  <div class="control">
+                    <input class="input" type="email" name="email" placeholder="Text input" aria-label="Your email" />
+                    <p class="help">This is a help text</p>
+                  </div>
+                </div>
+
+                <div class="field">
+                  <label for="message" class="label">Message</label>
+                  <div class="control">
+                    <textarea class="textarea" name="message" placeholder="Your message"></textarea>
+                  </div>
+                </div>
+
+                <div class="field is-grouped">
+                  <div class="control">
+                    <button type="submit" class="button is-primary">Submit</button>
+                  </div>
+                </div>
+              </fieldset>
+            </form>
           </div>
-        </fieldset>
-      </form>
-    </div>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  layout: 'about',
+}
 </script>
 
 <style></style>
