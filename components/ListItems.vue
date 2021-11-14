d1dfe08545c5a9247774ff61e77c35ceb95cd2d5
Scripted Pixels <kam@scriptedpixels.co.uk>
2021-11-14 08:57:52 +0000

    remove bulma classes

--- components/ListItems.vue
+++ components/ListItems.vue
@@ -4,8 +4,8 @@
       {{ postData.title }}
     </h3>
     <p class="subtitle">{{ postData.description }}</p>
-    <p class="is-light is-size-6 mb-0">Date published: {{ postData.date }}</p>
-    <p v-if="postData.category" class="is-light is-size-6 mb-0">Category: {{ postData.category }}</p>
+    <p>Date published: {{ postData.date }}</p>
+    <p v-if="postData.category">Category: {{ postData.category }}</p>
     <small v-if="postData.tags" class="tags has-addons is-size-6 mb-0">
       Tags:
       <span v-for="tag in postData.tags" :key="tag" class="tag is-white mb-0 pr-0 is-size-6">{{ tag }}</span>
