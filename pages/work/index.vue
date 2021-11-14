c2f7fa00b2c51baff2b9ff844b2ee057fdcb68be
Scripted Pixels <kam@scriptedpixels.co.uk>
2021-11-14 08:59:05 +0000

    remove bulma classes and tidy markup

--- pages/work/index.vue
+++ pages/work/index.vue
@@ -1,19 +1,11 @@
 <template>
   <div>
-    <section class="hero is-light">
-      <div class="hero-body">
-        <div class="container">
-          <h3 class="title is-size-3 is-spaced p-0 mb-0">Latest projects I've worked on</h3>
-        </div>
-      </div>
+    <section>
+      <h3 class="title is-size-3 is-spaced p-0 mb-0">Latest projects I've worked on</h3>
     </section>
-    <section class="section grid-content container">
-      <div class="content">
-        <div class="columns is-multiline">
-          <div v-for="post in workPosts" :key="post.slug" class="column is-full">
-            <ListItem :post-data="post" :route="path" />
-          </div>
-        </div>
+    <section>
+      <div v-for="post in workPosts" :key="post.slug" class="column is-full">
+        <ListItem :post-data="post" :route="path" />
       </div>
     </section>
   </div>
@@ -48,5 +40,3 @@ export default {
   layout: 'post',
 }
 </script>
-
-<style></style>
