27f9b254e5f69a59fd3119df1f1c2c8050db2b01
Scripted Pixels <kam@scriptedpixels.co.uk>
2021-11-16 16:59:47 +0000

    import footer into default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,16 +1,21 @@
 <template>
   <div>
     <Navigation />
-    <Nuxt />
+    <section class="max-w-7xl mx-auto px-4 sm:px-3 pt-12">
+      <Nuxt />
+    </section>
+    <Footer />
   </div>
 </template>
 
 <script>
 import Navigation from '@/components/TheNavbar'
+import Footer from '@/components/TheFooter'
 
 export default {
   components: {
     Navigation,
+    Footer,
   },
 }
 </script>
