8040a8f00aa38996c177168c7cee6862b9c3a8e7
Scripted Pixels <kam@scriptedpixels.co.uk>
2021-11-16 17:01:45 +0000

    fix header to top and and comment out small logo

--- components/TheNavbar.vue
+++ components/TheNavbar.vue
@@ -1,17 +1,17 @@
 <template>
-  <header class="w-screen border-b-2 border-gray-900">
+  <header class="fixed w-screen border-b border-gray-900 h-12 top-0 z-50">
     <div class="max-w-7xl mx-auto px-4 sm:px-3">
       <div class="flex justify-between items-center py-3">
-        <nuxt-link to="/">
+        <!-- <nuxt-link to="/">
           <img
             src="~/assets/images/scriptedpixelslogoInitial--white.svg"
             width="150"
             height="85"
             aria-hidden=""
             alt="Scripted Pixes Logo"
-            class="w-12 md:w-20"
+            class="w-10"
           />
-        </nuxt-link>
+        </nuxt-link> -->
 
         <nav role="navigation" aria-label="main navigation">
           <div class="text-white" :class="[isActive ? 'is-active' : '']">
