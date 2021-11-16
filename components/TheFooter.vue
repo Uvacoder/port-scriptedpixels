073d17732131045727fad3ed0e87660c77c9a981
Scripted Pixels <kam@scriptedpixels.co.uk>
2021-11-16 16:59:32 +0000

    add footer copy and markup for site

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -1,5 +1,14 @@
 <template>
-  <footer class="footer"></footer>
+  <footer class="flex justify-between max-w-7xl mx-auto px-4 sm:px-3 mt-6 pb-6 text-gray-600">
+    <p>&copy; Scripted Pixels Ltd 2021</p>
+
+    <p>No tracking cookies is used on this site.</p>
+
+    <ul class="flex gap-5">
+      <li>Twitter</li>
+      <li>LinkedIn</li>
+    </ul>
+  </footer>
 </template>
 
 <script>
