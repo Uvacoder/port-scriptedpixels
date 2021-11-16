e80e3a44b07e4347326eaecc32bde0ece9fc1ecf
Scripted Pixels <kam@scriptedpixels.co.uk>
2021-11-16 17:00:09 +0000

    fix heading level for posts

--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -1,12 +1,16 @@
 <template>
   <section>
-    <h1 class="title mb-0">{{ blogPost.title }}</h1>
+    <h4 class="title mb-0">{{ blogPost.title }}</h4>
+
     <p class="is-size-6">Date posted: {{ blogPost.date }}</p>
+
     <p v-if="blogPost.category" class="is-size-6">Category: {{ blogPost.category }}</p>
+
     <p v-if="blogPost.tags" class="tags has-addons is-size-6">
       Tags:
       <span v-for="tag in blogPost.tags" :key="tag" class="tag is-light mb-0 pr-0 is-size-6">{{ tag }}</span>
     </p>
+
     <div class="content">
       <nuxt-content :document="blogPost" />
     </div>
