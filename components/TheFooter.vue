9caad1479fb175a3e8e4b7751edd4ff312064a55
Scripted Pixels <kam@scriptedpixels.co.uk>
2021-11-18 17:31:43 +0000

    fixes footer style for mobile

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -1,8 +1,10 @@
 <template>
-  <footer class="flex justify-between max-w-7xl mx-auto px-4 sm:px-3 mt-6 pb-6 text-white opacity-50">
-    <p>&copy; Scripted Pixels Ltd 2021</p>
+  <footer
+    class="flex flex-col md:flex-row justify-between max-w-7xl mx-auto px-4 sm:px-3 mt-6 pb-6 text-white opacity-50"
+  >
+    <p class="mb-3">&copy; Scripted Pixels Ltd 2021</p>
 
-    <p>No tracking cookies are used on this site :)</p>
+    <p class="mb-3">No tracking cookies are used on this site :)</p>
 
     <ul class="flex gap-5">
       <li><a href="https://www.twitter.com/scriptedpixels" target="_blank">Twitter</a></li>
