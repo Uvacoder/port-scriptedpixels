62d355ec26c6a0b4590fce66c8b98c5cf9ac8031
Scripted Pixels <kam@scriptedpixels.co.uk>
2021-11-18 12:51:42 +0000

    Style list items in blog and work

--- components/ListItems.vue
+++ components/ListItems.vue
@@ -1,15 +1,19 @@
 <template>
-  <li class="mb-10">
+  <li class="mb-20">
     <nuxt-link :to="`${route}/${postData.slug}`">
-      <h4 class="title my-10 text-2xl">
+      <h4 class="title mt-0 mb-4 text-4xl">
         {{ postData.title }}
       </h4>
+
       <p class="font-light">{{ postData.description }}</p>
+
       <p>Date published: {{ postData.date }}</p>
+
       <p v-if="postData.category">Category: {{ postData.category }}</p>
+
       <small v-if="postData.tags" class="tags has-addons is-size-6 mb-0">
         Tags:
-        <span v-for="tag in postData.tags" :key="tag" class="tag is-white mb-0 pr-0 is-size-6">{{ tag }}</span>
+        <span v-for="tag in postData.tags" :key="tag" class="tag is-white mb-0 pr-0 is-size-6">{{ tag }}, </span>
       </small>
     </nuxt-link>
   </li>
