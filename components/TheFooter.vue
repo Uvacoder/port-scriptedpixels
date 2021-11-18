6bef6a14a5d34307c7bac0c306e3e110b5ac0e27
Scripted Pixels <kam@scriptedpixels.co.uk>
2021-11-18 12:51:51 +0000

    cleans up footer copy and styling

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -1,12 +1,12 @@
 <template>
-  <footer class="flex justify-between max-w-7xl mx-auto px-4 sm:px-3 mt-6 pb-6 text-gray-600">
+  <footer class="flex justify-between max-w-7xl mx-auto px-4 sm:px-3 mt-6 pb-6 text-white opacity-50">
     <p>&copy; Scripted Pixels Ltd 2021</p>
 
-    <p>No tracking cookies is used on this site.</p>
+    <p>No tracking cookies are used on this site :)</p>
 
     <ul class="flex gap-5">
-      <li>Twitter</li>
-      <li>LinkedIn</li>
+      <li><a href="https://www.twitter.com/scriptedpixels" target="_blank">Twitter</a></li>
+      <li><a href="https://www.linkedin.com/in/kambanwait" target="_blank">LinkedIn</a></li>
     </ul>
   </footer>
 </template>
