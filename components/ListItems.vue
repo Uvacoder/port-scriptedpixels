ca33ab82f0c21d916bbb3da6085802ad38fcc454
Scripted Pixels <kam@scriptedpixels.co.uk>
2021-11-22 18:10:19 +0000

    Styles main work/blog list items

--- components/ListItems.vue
+++ components/ListItems.vue
@@ -5,22 +5,33 @@
         {{ postData.title }}
       </h4>
 
-      <p class="font-light">{{ postData.description }}</p>
+      <p class="font-light text-xl mb-4">{{ postData.description }}</p>
 
-      <p>Date published: {{ postData.date }}</p>
+      <section class="border border-dashed border-b-0 border-l-0 border-r-0 pt-5 border-brandGray-200">
+        <p class="mb-2 text-sm"><span class="font-semibold">Date posted:</span> {{ postData.date }}</p>
 
-      <p v-if="postData.category">Category: {{ postData.category }}</p>
+        <p v-if="postData.category" class="mb-2 text-sm">
+          <span class="font-semibold">Category:</span> {{ postData.category }}
+        </p>
 
-      <small v-if="postData.tags" class="tags has-addons is-size-6 mb-0">
-        Tags:
-        <span v-for="tag in postData.tags" :key="tag" class="tag is-white mb-0 pr-0 is-size-6">{{ tag }}, </span>
-      </small>
+        <p v-if="postData.tags" class="mb-2 text-sm">
+          <span class="font-semibold">Tags:</span>
+          <span v-for="(tag, i) in postData.tags" :key="tag + i" :class="i + 1 == tagsTotal ? '' : 'mr-2'"
+            >{{ tag }}{{ i + 1 == tagsTotal ? '' : ',' }}</span
+          >
+        </p>
+      </section>
     </nuxt-link>
   </li>
 </template>
 
 <script>
 export default {
-  props: ['postData', 'route']
+  props: ['postData', 'route'],
+  computed: {
+    tagsTotal() {
+      return this.postData.tags ? this.postData.tags.length : 0
+    }
+  }
 }
 </script>
