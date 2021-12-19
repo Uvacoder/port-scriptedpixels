9c32d48579041dc0b81b068111b563b1cc19dcfc
Scripted Pixels <kam@scriptedpixels.co.uk>
2021-12-19 16:00:20 +0000

    if a post is archived and has no post page, then click will open the site in a new tab
    else, open the slug page for the post

--- components/ListItems.vue
+++ components/ListItems.vue
@@ -1,6 +1,34 @@
 <template>
   <li class="mb-20">
-    <nuxt-link :to="`${route}/${postData.slug}`">
+    <a
+      v-if="postData.archivedLink"
+      target="_blank"
+      rel="noopener noreferrer"
+      :href="postData.archivedLink"
+      title="postData.title"
+    >
+      <h4 class="mt-0 mb-4 text-4xl">
+        {{ postData.title }}
+      </h4>
+
+      <p class="font-light text-xl mb-4">{{ postData.description }}</p>
+
+      <section class="border border-dashed border-b-0 border-l-0 border-r-0 pt-5 border-brandGray-200">
+        <p class="mb-2 text-sm"><span class="font-semibold">Date posted:</span> {{ formattedDate }}</p>
+
+        <p v-if="postData.category" class="mb-2 text-sm">
+          <span class="font-semibold">Category:</span> {{ postData.category }}
+        </p>
+
+        <p v-if="postData.tags" class="mb-2 text-sm">
+          <span class="font-semibold">Tags:</span>
+          <span v-for="(tag, i) in postData.tags" :key="tag + i" :class="i + 1 == tagsTotal ? '' : 'mr-2'"
+            >{{ tag }}{{ i + 1 == tagsTotal ? '' : ',' }}</span
+          >
+        </p>
+      </section>
+    </a>
+    <nuxt-link v-else :to="`${route}/${postData.slug}`">
       <h4 class="mt-0 mb-4 text-4xl">
         {{ postData.title }}
       </h4>
