3b22a01454dde1fb44abe5ce07db053ad058df52
Scripted Pixels <kam@scriptedpixels.co.uk>
2021-12-30 15:18:18 +0000

    remove hanging ,

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,15 +3,15 @@ module.exports = {
   env: {
     browser: true,
     node: true,
-    es6: true,
+    es6: true
   },
   parserOptions: {
-    parser: 'babel-eslint',
+    parser: 'babel-eslint'
   },
   extends: ['@nuxtjs', 'prettier', 'prettier/vue', 'plugin:prettier/recommended', 'plugin:nuxt/recommended'],
   plugins: ['prettier'],
   // add your custom rules here
   rules: {
-    quotes: [0, 'single', { avoidEscape: true }],
-  },
+    quotes: [0, 'single', { avoidEscape: true }]
+  }
 }
