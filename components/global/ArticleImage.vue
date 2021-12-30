acc0d5dc272cd4465e132d8c9a8c76fbea4e7cf1
Scripted Pixels <kam@scriptedpixels.co.uk>
2021-12-30 14:36:55 +0000

    adds global component for images to be displayed in posts

---
+++ components/global/ArticleImage.vue
@@ -0,0 +1,34 @@
+<template>
+  <img :src="imgSrc()" :alt="getAlt()" class="article-image" />
+</template>
+
+<script>
+export default {
+  props: {
+    src: {
+      type: String,
+      required: true
+    },
+    alt: {
+      type: String,
+      required: true
+    }
+  },
+  methods: {
+    imgSrc() {
+      try {
+        return require(`~/assets/images/${this.src}`)
+      } catch (e) {
+        return null
+      }
+    },
+    getAlt() {
+      try {
+        return this.alt
+      } catch (e) {
+        return 'Please provide image alt text'
+      }
+    }
+  }
+}
+</script>
