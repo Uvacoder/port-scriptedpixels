ac0721e25283d7bd62859d149bd2570481afd6ed
Scripted Pixels <kam@scriptedpixels.co.uk>
2021-12-30 15:37:47 +0000

    removes extra right margin on nac link

--- components/TheNavbar.vue
+++ components/TheNavbar.vue
@@ -1,24 +1,41 @@
 <template>
-  <header class="fixed w-screen border-b border-gray-900 h-12 top-0 z-50 drop-shadow-md">
+  <header
+    class="fixed w-screen border-b border-gray-900 h-12 top-0 z-50 drop-shadow-md"
+  >
     <div class="max-w-7xl mx-auto px-3 md:px-6">
       <div class="flex justify-center md:justify-start items-center py-3">
-        <!-- <nuxt-link to="/" class="md:ml-0 md:mr-auto">
+        <!-- <nuxt-link to="/" class="ml-0 mr-auto">
           <img
             src="~/assets/images/scriptedpixelslogoInitial--white.svg"
             width="150"
             height="85"
             aria-hidden=""
             alt="Scripted Pixes Logo"
-            class="w-7 hidden md:block"
+            class="w-7 md:block"
           />
         </nuxt-link> -->
 
         <nav role="navigation" aria-label="main navigation" class="text-white">
           <template :class="[isActive ? 'is-active' : '']">
-            <nuxt-link class="tracking-widest mr-3 md:mr-3 text-lg" exact :to="{ name: 'index' }">Home</nuxt-link>
-            <nuxt-link class="tracking-widest mr-3 md:mr-3 text-lg" :to="{ name: 'about' }">About</nuxt-link>
-            <nuxt-link class="tracking-widest mr-3 md:mr-3 text-lg" :to="{ name: 'work' }">Work</nuxt-link>
-            <nuxt-link class="tracking-widest mr-3 text-lg" :to="{ name: 'blog' }">Blog</nuxt-link>
+            <nuxt-link
+              class="tracking-widest mr-3 md:mr-3 text-lg"
+              exact
+              :to="{ name: 'index' }"
+              >Home</nuxt-link
+            >
+            <nuxt-link
+              class="tracking-widest mr-3 md:mr-3 text-lg"
+              :to="{ name: 'about' }"
+              >About</nuxt-link
+            >
+            <nuxt-link
+              class="tracking-widest mr-3 md:mr-3 text-lg"
+              :to="{ name: 'work' }"
+              >Work</nuxt-link
+            >
+            <nuxt-link class="tracking-widest text-lg" :to="{ name: 'blog' }"
+              >Blog</nuxt-link
+            >
           </template>
         </nav>
       </div>
