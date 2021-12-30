74c74ecab099f652ae96664a3984602df98b2eaf
Scripted Pixels <kam@scriptedpixels.co.uk>
2021-12-30 14:42:15 +0000

    decrease font size in footer

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -2,11 +2,11 @@
   <footer
     class="flex flex-col md:flex-row justify-between max-w-7xl mx-auto px-3 md:px-6 mt-6 pb-6 text-white opacity-50"
   >
-    <p class="mb-3">&copy; Scripted Pixels Ltd 2021</p>
+    <p class="mb-3 text-xs">&copy; Scripted Pixels Ltd 2021</p>
 
-    <p class="mb-3">No tracking cookies are used on this site :)</p>
+    <p class="mb-3 text-xs">No tracking cookies are used on this site :)</p>
 
-    <ul class="flex gap-5">
+    <ul class="flex gap-5 text-xs">
       <li><a href="https://www.twitter.com/scriptedpixels" target="_blank">Twitter</a></li>
       <li><a href="https://www.linkedin.com/in/kambanwait" target="_blank">LinkedIn</a></li>
     </ul>
