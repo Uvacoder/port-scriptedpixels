4b37691f05e35e9b9a4c92dd7ddb8ca4ebd0cb42
Scripted Pixels <kam@scriptedpixels.co.uk>
2021-12-31 14:16:36 +0000

    Adds work status in footer

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -1,14 +1,27 @@
 <template>
   <footer
-    class="flex flex-col md:flex-row justify-between max-w-7xl mx-auto px-3 md:px-6 mt-6 pb-6 text-white opacity-50"
+    class="flex flex-col md:flex-row justify-between max-w-7xl mx-auto px-3 md:px-6 mt-6 pb-6 text-white opacity-50 md:items-center"
   >
-    <p class="mb-3 text-xs">&copy; Scripted Pixels Ltd 2021</p>
+    <p class="mb-3 md:mb-0 text-xs mx-1">&copy; Scripted Pixels Ltd 2021</p>
 
-    <p class="mb-3 text-xs">No tracking cookies are used on this site :)</p>
+    <p class="mb-3 md:mb-0 text-xs mx-1">
+      No tracking cookies are used on this site :)
+    </p>
 
-    <ul class="flex gap-5 text-xs">
-      <li><a href="https://www.twitter.com/scriptedpixels" target="_blank">Twitter</a></li>
-      <li><a href="https://www.linkedin.com/in/kambanwait" target="_blank">LinkedIn</a></li>
+    <ul class="flex gap-5 text-xs items-center mx-1">
+      <li class="bg-red-700 p-1 px-2 rounded font-semibold">
+        Currently not available for work
+      </li>
+      <li>
+        <a href="https://www.twitter.com/scriptedpixels" target="_blank"
+          >Twitter</a
+        >
+      </li>
+      <li>
+        <a href="https://www.linkedin.com/in/kambanwait" target="_blank"
+          >LinkedIn</a
+        >
+      </li>
     </ul>
   </footer>
 </template>
