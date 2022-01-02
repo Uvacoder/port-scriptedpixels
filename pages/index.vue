463ad42c0b2130f456ba2ddfa621c3cc3a37af1f
Scripted Pixels <kam@scriptedpixels.co.uk>
2022-01-02 10:16:38 +0000

    cleans up index vue page

--- pages/index.vue
+++ pages/index.vue
@@ -2,59 +2,19 @@
   <section class="flex flex-col justify-center min-h-screen -mt-12 -mb-20">
     <div class="mx-auto px-4 sm:px-3">
       <img src="../assets/images/scriptedpixels-logo--white.svg" alt="" />
-      <h2 ref="subHeading" class="text-center text-l text-white md:text-2xl my-0 mt-5">Design, Develop, Deliver</h2>
+      <h2 class="text-center text-l text-white md:text-2xl my-0 mt-5 light">
+        Design, Develop, Deliver
+      </h2>
     </div>
   </section>
-  <!--  -->
-  <!-- <section class="flex flex-col justify-center h-screen"> -->
-  <!-- <div class="column">
-        <h3 class="title title--with-bottom-border title--border-danger is-spaced">Design</h3>
-        <h4 class="subtitle">Wireframes, Sketch, Prototypes</h4>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum nisi corporis eveniet ipsam blanditiis in amet,
-          eligendi, optio earum totam similique eius nostrum. Voluptatem facilis possimus saepe molestias nisi cum.
-          <br />
-          {{ test }}
-        </p>
-      </div>
-
-      <div class="column">
-        <h3 class="title title--with-bottom-border title--border-link is-spaced">Develop</h3>
-        <h4 class="subtitle">Coding, iterate, create</h4>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum nisi corporis eveniet ipsam blanditiis in amet,
-          eligendi, optio earum totam similique eius nostrum. Voluptatem facilis possimus saepe molestias nisi cum.
-        </p>
-      </div>
-
-      <div class="column">
-        <h3 class="title title--with-bottom-border title--border-success is-spaced">Deliver</h3>
-        <h4 class="subtitle">Deploy, release, production</h4>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum nisi corporis eveniet ipsam blanditiis in amet,
-          eligendi, optio earum totam similique eius nostrum. Voluptatem facilis possimus saepe molestias nisi cum.
-        </p>
-      </div>
-    </div> -->
-  <!-- </section> -->
 </template>
 
 <script>
 export default {
-  asyncData({ $config }) {
-    // eslint-disable-next-line no-console
-    const test = $config.apiSecret
-  },
   head() {
     return {
-      title: 'Scripted Pixels | Home Page',
+      title: 'Scripted Pixels | Home Page'
     }
-  },
+  }
 }
 </script>
-
-<style scoped lang="scss">
-.columns {
-  align-items: stretch;
-}
-</style>
