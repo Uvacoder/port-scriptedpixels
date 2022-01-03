6c1276aff8f67b2f151b0ad97acd23dca60234ac
Scripted Pixels <kam@scriptedpixels.co.uk>
2022-01-03 11:24:52 +0000

    removes archive linked logic and adds work specific date format

--- components/ListItems.vue
+++ components/ListItems.vue
@@ -1,13 +1,6 @@
 <template>
   <li class="mb-20">
-    <a
-      v-if="postData.archivedLink"
-      target="_blank"
-      rel="noopener noreferrer"
-      :href="postData.archivedLink"
-      title="postData.title"
-      class="block"
-    >
+    <nuxt-link :to="`${route}/${postData.slug}`">
       <h4 class="mt-0 mb-4 text-3xl md:text-4xl">
         {{ postData.title }}
       </h4>
@@ -18,40 +11,8 @@
         class="border border-dashed border-b-0 border-l-0 border-r-0 pt-5 border-brandGray-200"
       >
         <p v-if="postData.category == 'Work'" class="mb-2 text-sm">
-          <span class="font-semibold">Date worked: </span> {{ formattedDate }}
-        </p>
-
-        <p v-else class="mb-2 text-sm">
-          <span class="font-semibold">Date posted:</span> {{ formattedDate }}
-        </p>
-
-        <p v-if="postData.category" class="mb-2 text-sm">
-          <span class="font-semibold">Category:</span> {{ postData.category }}
-        </p>
-
-        <p v-if="postData.tags" class="mb-2 text-sm">
-          <span class="font-semibold">Tags:</span>
-          <span
-            v-for="(tag, i) in postData.tags"
-            :key="tag + i"
-            :class="i + 1 == tagsTotal ? '' : 'mr-2'"
-            >{{ tag }}{{ i + 1 == tagsTotal ? '' : ',' }}</span
-          >
-        </p>
-      </section>
-    </a>
-    <nuxt-link v-else :to="`${route}/${postData.slug}`">
-      <h4 class="mt-0 mb-4 text-3xl md:text-4xl">
-        {{ postData.title }}
-      </h4>
-
-      <p class="font-light text-xl mb-4">{{ postData.description }}</p>
-
-      <section
-        class="border border-dashed border-b-0 border-l-0 border-r-0 pt-5 border-brandGray-200"
-      >
-        <p v-if="postData.category == 'Work'" class="mb-2 text-sm">
-          <span class="font-semibold">Date worked: </span> {{ formattedDate }}
+          <span class="font-semibold">Date worked: </span>
+          {{ postData.dateWorked }}
         </p>
 
         <p v-else class="mb-2 text-sm">
@@ -94,16 +55,30 @@ export default {
     },
     formattedDate() {
       const options = {
-        weekday: 'long',
         year: 'numeric',
         month: 'long',
+        weekday: 'long',
         day: 'numeric'
       }
+
       const dateTimeFormat = new Intl.DateTimeFormat('en-gb', options)
       const date = new Date(this.postData.date)
       const formatDate = dateTimeFormat.format(date)
 
       return formatDate
+    }
+  },
+  methods: {
+    formatDate() {
+      const options = {
+        year: 'numeric',
+        month: 'long'
+      }
+      const dateTimeFormat = new Intl.DateTimeFormat('en-gb', options)
+      const date = new Date(this.postData.date)
+      const dateFormatted = dateTimeFormat.format(date)
+
+      return dateFormatted
     }
   }
 }
