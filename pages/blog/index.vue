fc8435a4983f25320347f0c78e9db476ea5f817f
Scripted Pixels <kam@scriptedpixels.co.uk>
2022-02-22 15:01:00 +0000

    Adds tags filter to blog posts

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,37 +1,34 @@
 <template>
-  <section class="flex max-w-7xl mx-auto px-3 md:px-6 pt-12">
-    <ul class="mt-28 mb-16 max-w-3xl mx-auto w-full">
+  <section
+    class="flex flex-wrap md:flex-nowrap gap-10 max-w-7xl mx-auto px-3 md:px-6 pt-12"
+  >
+    <aside class="mt-10 w-full md:w-1/5">
+      <h5 class="title mt-0 mb-4 text-xl">Filter posts:</h5>
+      <ul>
+        <li
+          v-for="(tag, index) in tags"
+          :key="tag + index"
+          class="inline mr-2 mb-2 md:block hover:cursor-pointer md:mb-1 md:mr-0"
+          :data-tag="tag"
+          :class="selectedTag == tag ? 'font-bold' : ''"
+          @click="setTag"
+        >
+          {{ tag }}
+        </li>
+      </ul>
+
+      <h5 class="title mt-10 mb-1 text-l">Stats</h5>
+      <p class="text-sm">{{ filteredBlogPosts.length }} Blogpost(s)</p>
+    </aside>
+
+    <ul class="md:mt-10 w-full md:w-4/5">
       <ListItem
-        v-for="post in blogPosts"
+        v-for="post in filteredBlogPosts"
         :key="post.slug"
         :post-data="post"
         :route="path"
       />
     </ul>
-
-    <!-- <aside class="mb-10 mt-28 mr-auto w-1/5">
-      <h5 class="title mt-10 mb-1 text-xl">Stats</h5>
-      <p>#{{ blogPosts.length }} Blogposts</p>
-
-      <h5 class="title mt-10 mb-1 text-xl">Search</h5>
-      <form>
-        <fieldset>
-          <input
-            type="text"
-            placeholder="Search the blog"
-            aria-label="Search the blog"
-          />
-          <button type="submit">Search</button>
-        </fieldset>
-      </form>
-
-      <h5 class="title -mt-0 mb-1 text-xl">Tags</h5>
-      <ul>
-        <li v-for="(tag, index) in tags" :key="tag + index" class="inline">
-          {{ tag }},
-        </li>
-      </ul>
-    </aside> -->
   </section>
 </template>
 
@@ -51,21 +48,22 @@ export default {
 
       const path = route.path
 
-      // const allTags = blogPosts.map((post) => {
-      //   const postTags = post.tags !== undefined ? post.tags : ''
-      //   const filterdPostTags = postTags.filter(
-      //     (postTag) => postTag !== undefined
-      //   )
+      const allTags = blogPosts.map((post) => {
+        const postTags = post.tags !== undefined ? post.tags : ''
 
-      //   return filterdPostTags
-      // })
+        const filterdPostTags = postTags.filter((postTag) => {
+          return postTag !== undefined
+        })
 
-      // const tags = [...new Set(allTags.flat(1))]
+        return filterdPostTags
+      })
+
+      const tags = [...new Set(allTags.flat(1))]
 
       return {
         blogPosts,
-        path
-        // tags
+        path,
+        tags
       }
     } catch (error) {
       // eslint-disable-next-line no-console
@@ -73,8 +71,31 @@ export default {
       return false
     }
   },
+  data() {
+    return {
+      tags: [],
+      selectedTag: ''
+    }
+  },
+  computed: {
+    filteredBlogPosts() {
+      const filteredPosts = this.blogPosts.filter((blogpost) => {
+        // eslint-disable-next-line no-console
+        if (!this.selectedTag || this.selectedTag.length === 0) return blogpost
+
+        if (blogpost.tags.includes(this.selectedTag)) {
+          return blogpost
+        }
+      })
+
+      return filteredPosts
+    }
+  },
   methods: {
-    // listTags(tags) {},
+    //   listTags(tags) {}
+    setTag(event) {
+      this.selectedTag = event.target.getAttribute('data-tag')
+    }
   },
   layout: 'blog'
 }
