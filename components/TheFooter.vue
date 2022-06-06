730396330657d0c43b4f63a1d4246380cc822e40
Scripted Pixels <kam@scriptedpixels.co.uk>
2022-06-06 10:31:24 +0100

    Adds Clicky tracking tag and updates footer copy

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -4,10 +4,8 @@
   >
     <p class="mb-3 md:mb-0 text-xs mx-1">
       <span class="block mb-2">&copy; Scripted Pixels Ltd 2021</span>
-      <a href="https://logrocket.com" target="_blank">LogRocket</a> &
-      <a href="https://sentry.io" target="_blank">Sentry</a> is used to monitor
-      performance/errors on the site 🤓
-    </p>
+
+      <a href="https://logrocket.com" target="_blank" rel="noreferrer noopener">LogRocket</a>, <a href="https://sentry.io" target="_blank" rel="noreferrer noopener">Sentry</a> & <a href="https://clicky.com" target="_blank" rel="noreferrer noopener">Clicky</a> are used to monitor performance, errors & Analytics on the site 🤓</p>
 
     <ul class="flex flex-wrap gap-5 text-xs items-center md:self-end mx-1">
       <li class="bg-red-700 p-1 px-2 rounded font-semibold">
@@ -15,15 +13,25 @@
       </li>
       <li>
         <a href="https://www.twitter.com/scriptedpixels" target="_blank"
-          >Twitter</a
-        >
+          >Twitter
+        </a>
       </li>
       <li>
         <a href="https://www.linkedin.com/in/kambanwait" target="_blank"
-          >LinkedIn</a
-        >
+          >LinkedIn
+        </a>
       </li>
     </ul>
+
+    <script async src="//static.getclicky.com/101368919.js"></script>
+    <noscript>
+      <img
+        src="//in.getclicky.com/101368919ns.gif"
+        alt="clicky tracking pixel"
+        width="1"
+        height="1"
+      />
+    </noscript>
   </footer>
 </template>
 
