3ab963fb8d844316adae81c726536ba7517086e6
Kam Banwait <kam@scriptedpixels.co.uk>
2022-08-22 15:06:33 +0100

    remove incorrectly added scripts from footer

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -26,11 +26,5 @@
         </a>
       </li>
     </ul>
-
-    <!-- <script async src="//static.getclicky.com/101368919.js"></script> -->
-    <!-- <noscript>
-      <img src="//in.getclicky.com/101368919ns.gif" alt="clicky tracking pixel"
-        width="1" height="1" />
-    </noscript> -->
   </footer>
 </template>
